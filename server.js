eed43863f602fd9cd1a513ee39800d79fa609ffd
sasho33 <sasho3333333@gmail.com>
2023-11-28 14:03:25 +0000

    get all posts and post by author

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.get('/api/quotes/random', (req, res, next) => {
-  const randomQuote = getRandomElement(quotes).quote;
-  console.log(randomQuote);
+  const randomQuote = getRandomElement(quotes);
   res.status(200).send({ quote: randomQuote });
+});
+
+app.get('/api/quotes/', (req, res, next) => {
+  const author = req.query.person;
+  if (!author) {
+    res.send({ quotes });
+  } else if (author) {
+    const quotesByAuthor = quotes.filter((quote) => quote.person === author);
+    res.send({ quotes: quotesByAuthor });
+  }
 });
 
 app.listen(PORT, (req, res, next) => {
